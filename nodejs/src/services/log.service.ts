d0203b58aa2d40c462b5ac538a0d92051f5ad731
Vactor0911 <vactor0911@gmail.com>
2026-01-03 06:29:40 +0900

    서비스 처리 로그 기록 로직 구현

---
+++ nodejs/src/services/log.service.ts
@@ -0,0 +1,39 @@
+import LogModel from "../models/log.model";
+import { v4 as uuidv4 } from "uuid";
+import { dbPool } from "../config/db";
+
+class LogService {
+  /**
+   * 사용자 생성 로그 기록
+   * @param apiKey 사용된 API 키
+   * @param userUuid 생성된 사용자 uuid
+   */
+  static async logUserCreation(apiKey: string, userUuid: string) {
+    const logUuid = uuidv4();
+    await LogModel.create(
+      logUuid,
+      apiKey,
+      "user",
+      `사용자 생성 : ${userUuid}`,
+      dbPool
+    );
+  }
+
+  /**
+   * 사용자명 재설정 로그 기록
+   * @param apiKey 사용된 API 키
+   * @param steamId 스팀 고유번호 (SteamID64)
+   */
+  static async logUserNameRefresh(apiKey: string, steamId: string) {
+    const logUuid = uuidv4();
+    await LogModel.create(
+      logUuid,
+      apiKey,
+      "user",
+      `사용자명 재설정 : ${steamId}`,
+      dbPool
+    );
+  }
+}
+
+export default LogService;
