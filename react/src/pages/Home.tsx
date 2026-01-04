710fada3c83f1a88ba9fe9e5872b792bb463042a
Vactor0911 <vactor0911@gmail.com>
2026-01-04 22:00:48 +0900

    React 기본 개발 틀 구성

---
+++ react/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <Typography variant="h4">Welcome to JBank</Typography>
+    </>
+  );
+};
+
+export default Home;
