d9d60eb5b7d1f26a7167051911be37b66571ac7b
Vactor0911 <vactor0911@gmail.com>
2026-01-04 13:57:40 +0900

    줄바꿈 추가

--- nodejs/src/schema/transaction.schema.ts
+++ nodejs/src/schema/transaction.schema.ts
@@ -1,7 +1,9 @@
+import z from "zod";
+
 /**
  * 거래 내역 조회 스키마
  */
-export const searchAccountTransactionsSchema = z.object({
+export const getTransactionsSchema = z.object({
   accountNumber: z
     .string()
     .regex(/^\d{4}-\d{4}$/, "계좌번호 형식이 올바르지 않습니다."),
