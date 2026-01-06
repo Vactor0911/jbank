487babab2016267dfd27f55040c09ce962315ed4
Vactor0911 <vactor0911@gmail.com>
2026-01-06 21:28:05 +0900

    헤더 구현

---
+++ react/src/components/Header.tsx
@@ -0,0 +1,57 @@
+import { IconButton, Stack, useTheme } from "@mui/material";
+import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
+import LanguageRoundedIcon from "@mui/icons-material/LanguageRounded";
+
+const Header = () => {
+  const theme = useTheme();
+  const isDarkMode = theme.palette.mode === "dark";
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="flex-end"
+      alignItems="center"
+      p={1}
+      gap={0.5}
+    >
+      {/* 언어 변경 버튼 */}
+      <IconButton>
+        <LanguageRoundedIcon
+          sx={{
+            fontSize: 32,
+          }}
+        />
+      </IconButton>
+
+      {/* 테마 변경 버튼 */}
+      <IconButton>
+        {isDarkMode ? (
+          <DarkModeRoundedIcon
+            sx={{
+              fontSize: 32,
+            }}
+          />
+        ) : (
+          <LightModeRoundedIcon
+            sx={{
+              fontSize: 32,
+            }}
+          />
+        )}
+      </IconButton>
+
+      {/* 알림 버튼 */}
+      <IconButton>
+        <NotificationsNoneRoundedIcon
+          sx={{
+            fontSize: 32,
+          }}
+        />
+      </IconButton>
+    </Stack>
+  );
+};
+
+export default Header;
