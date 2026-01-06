afbf40a0a1d19a52bb5166e2047e86d9d54ffd50
Vactor0911 <vactor0911@gmail.com>
2026-01-07 02:33:45 +0900

    헤더 컴포넌트 크기 할당 오류 수정

--- react/src/components/Header.tsx
+++ react/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Stack, useTheme } from "@mui/material";
+import { IconButton, Stack, useTheme } from "@mui/material";
 import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
 import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
 import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
@@ -9,61 +9,55 @@ const Header = () => {
   const isDarkMode = theme.palette.mode === "dark";
 
   return (
-    <>
-      {/* 헤더 공간 차지용 div */}
-      <Box minHeight="64px" flexShrink={0} />
+    <Stack
+      direction="row"
+      width="100%"
+      height="64px"
+      justifyContent="flex-end"
+      alignItems="center"
+      p={1}
+      gap={0.5}
+      position="sticky"
+      top={0}
+      right={0}
+      bgcolor="#F6F7F9"
+      zIndex={1000}
+    >
+      {/* 언어 변경 버튼 */}
+      <IconButton>
+        <LanguageRoundedIcon
+          sx={{
+            fontSize: 32,
+          }}
+        />
+      </IconButton>
 
-      {/* 헤더 */}
-      <Stack
-        direction="row"
-        width="100%"
-        height="64px"
-        justifyContent="flex-end"
-        alignItems="center"
-        p={1}
-        gap={0.5}
-        position="fixed"
-        top={0}
-        right={0}
-        bgcolor="#F6F7F9"
-        zIndex={1000}
-      >
-        {/* 언어 변경 버튼 */}
-        <IconButton>
-          <LanguageRoundedIcon
+      {/* 테마 변경 버튼 */}
+      <IconButton>
+        {isDarkMode ? (
+          <DarkModeRoundedIcon
             sx={{
               fontSize: 32,
             }}
           />
-        </IconButton>
-
-        {/* 테마 변경 버튼 */}
-        <IconButton>
-          {isDarkMode ? (
-            <DarkModeRoundedIcon
-              sx={{
-                fontSize: 32,
-              }}
-            />
-          ) : (
-            <LightModeRoundedIcon
-              sx={{
-                fontSize: 32,
-              }}
-            />
-          )}
-        </IconButton>
-
-        {/* 알림 버튼 */}
-        <IconButton>
-          <NotificationsNoneRoundedIcon
+        ) : (
+          <LightModeRoundedIcon
             sx={{
               fontSize: 32,
             }}
           />
-        </IconButton>
-      </Stack>
-    </>
+        )}
+      </IconButton>
+
+      {/* 알림 버튼 */}
+      <IconButton>
+        <NotificationsNoneRoundedIcon
+          sx={{
+            fontSize: 32,
+          }}
+        />
+      </IconButton>
+    </Stack>
   );
 };
 
