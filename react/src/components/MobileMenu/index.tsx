e3345706bbf806040a646445c1b28822ba5a14c1
Vactor0911 <vactor0911@gmail.com>
2026-01-07 04:16:44 +0900

    모바일 전용 메뉴 반응형 래퍼 컴포넌트 추가

--- react/src/components/MobileMenu/index.tsx
+++ react/src/components/MobileMenu/index.tsx
@@ -1,6 +1,7 @@
 import {
   Avatar,
   ButtonBase,
+  Container,
   Stack,
   Typography,
   useMediaQuery,
@@ -32,69 +33,75 @@ const MobileMenu = () => {
   }
 
   return (
-    <Stack width="100%" flex={1} px={2} pb={3} gap={3}>
-      {/* 프로필 정보 */}
-      <ButtonBase
-        sx={{
-          width: "100%",
-          textAlign: "left",
-          p: "2px 4px",
-          borderRadius: 2,
-        }}
-        onClick={handleProfileClick}
-      >
-        <Stack direction="row" width="100%" gap={2} alignItems="center">
-          {/* 프로필 이미지 */}
-          <Avatar
-            src={SampleProfileImage}
-            variant="rounded"
-            sx={{
-              width: "40px",
-              height: "40px",
-            }}
-          />
+    <Container
+      maxWidth="md"
+      sx={{
+        pb: 3,
+      }}
+    >
+      <Stack width="100%" flex={1} gap={3}>
+        {/* 프로필 정보 */}
+        <ButtonBase
+          sx={{
+            width: "100%",
+            textAlign: "left",
+            p: "2px 4px",
+            borderRadius: 2,
+          }}
+          onClick={handleProfileClick}
+        >
+          <Stack direction="row" width="100%" gap={2} alignItems="center">
+            {/* 프로필 이미지 */}
+            <Avatar
+              src={SampleProfileImage}
+              variant="rounded"
+              sx={{
+                width: "40px",
+                height: "40px",
+              }}
+            />
 
-          {/* 사용자 정보 */}
-          <Stack flex={1} overflow="hidden">
-            {/* Jbank 사용자 닉네임 */}
-            <Typography
-              variant="body1"
-              fontWeight={500}
-              noWrap
-              textOverflow="ellipsis"
-            >
-              백터
-            </Typography>
+            {/* 사용자 정보 */}
+            <Stack flex={1} overflow="hidden">
+              {/* Jbank 사용자 닉네임 */}
+              <Typography
+                variant="body1"
+                fontWeight={500}
+                noWrap
+                textOverflow="ellipsis"
+              >
+                백터
+              </Typography>
 
-            {/* Steam 닉네임 */}
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              noWrap
-              textOverflow="ellipsis"
-            >
-              백터 (Vactor0911)
-            </Typography>
+              {/* Steam 닉네임 */}
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                noWrap
+                textOverflow="ellipsis"
+              >
+                백터 (Vactor0911)
+              </Typography>
+            </Stack>
+
+            {/* 아이콘 */}
+            <NavigateNextOutlinedIcon />
           </Stack>
+        </ButtonBase>
 
-          {/* 아이콘 */}
-          <NavigateNextOutlinedIcon />
-        </Stack>
-      </ButtonBase>
+        {/* 송금하기 */}
+        <LinkButtonContainer label="송금하기" linkTo="/transfer" />
 
-      {/* 링크 버튼 컨테이너 */}
-      {/* 송금하기 */}
-      <LinkButtonContainer label="송금하기" linkTo="/transfer" />
+        {/* 계좌 */}
+        <AccountLinkButtonContainer />
 
-      {/* 계좌 */}
-      <AccountLinkButtonContainer />
+        {/* Ads (광고) */}
+        <AdsContainer />
 
-      {/* Ads (광고) */}
-      <AdsContainer />
-
-      {/* 푸터 */}
-      <Footer />
-    </Stack>
+        {/* 푸터 */}
+        <Footer />
+      </Stack>
+    </Container>
   );
 };
 
