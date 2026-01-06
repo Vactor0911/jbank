b108959846fcf0c5f21a9488b5da9e9e36e3f16a
Vactor0911 <vactor0911@gmail.com>
2026-01-07 04:54:51 +0900

    PC 및 모바일 메뉴 탭 이동 기능 구현

--- react/src/components/MobileMenu/index.tsx
+++ react/src/components/MobileMenu/index.tsx
@@ -1,30 +1,15 @@
-import {
-  Avatar,
-  ButtonBase,
-  Container,
-  Stack,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined";
-import SampleProfileImage from "../../assets/sample-user-profile.png";
-import { useLocation, useNavigate } from "react-router";
-import LinkButtonContainer from "./LinkButtonContainer";
-import AccountLinkButtonContainer from "./AccountLinkButtonContainer";
-import { useCallback } from "react";
-import AdsContainer from "./AdsContainer";
+import { Container, Stack, useMediaQuery, useTheme } from "@mui/material";
+import { useLocation } from "react-router";
 import Footer from "../Footer";
+import { useAtomValue } from "jotai";
+import { navigationValueAtom } from "../../states";
+import { Home } from "./Tabs";
 
 const MobileMenu = () => {
   const theme = useTheme();
-  const navigate = useNavigate();
   const location = useLocation();
 
-  // 프로필 정보 클릭 핸들러
-  const handleProfileClick = useCallback(() => {
-    navigate("/profile");
-  }, [navigate]);
+  const navigationValue = useAtomValue(navigationValueAtom);
 
   // 모바일 화면에서만 표시
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
@@ -36,67 +21,12 @@ const MobileMenu = () => {
     <Container
       maxWidth="md"
       sx={{
-        pb: 3,
+        height: "100%",
+        mb: 5,
       }}
     >
-      <Stack width="100%" flex={1} gap={3}>
-        {/* 프로필 정보 */}
-        <ButtonBase
-          sx={{
-            width: "100%",
-            textAlign: "left",
-            p: "2px 4px",
-            borderRadius: 2,
-          }}
-          onClick={handleProfileClick}
-        >
-          <Stack direction="row" width="100%" gap={2} alignItems="center">
-            {/* 프로필 이미지 */}
-            <Avatar
-              src={SampleProfileImage}
-              variant="rounded"
-              sx={{
-                width: "40px",
-                height: "40px",
-              }}
-            />
-
-            {/* 사용자 정보 */}
-            <Stack flex={1} overflow="hidden">
-              {/* Jbank 사용자 닉네임 */}
-              <Typography
-                variant="body1"
-                fontWeight={500}
-                noWrap
-                textOverflow="ellipsis"
-              >
-                백터
-              </Typography>
-
-              {/* Steam 닉네임 */}
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                noWrap
-                textOverflow="ellipsis"
-              >
-                백터 (Vactor0911)
-              </Typography>
-            </Stack>
-
-            {/* 아이콘 */}
-            <NavigateNextOutlinedIcon />
-          </Stack>
-        </ButtonBase>
-
-        {/* 송금하기 */}
-        <LinkButtonContainer label="송금하기" linkTo="/transfer" />
-
-        {/* 계좌 */}
-        <AccountLinkButtonContainer />
-
-        {/* Ads (광고) */}
-        <AdsContainer />
+      <Stack width="100%" gap={3}>
+        {navigationValue === 0 && <Home />}
 
         {/* 푸터 */}
         <Footer />
--- react/src/states/index.ts
+++ react/src/states/index.ts
@@ -1,3 +1,6 @@
 import { atom } from "jotai";
 
+/**
+ * 네비게이션 바 선택 값 상태
+ */
 export const navigationValueAtom = atom(0);
