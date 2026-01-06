e121b699601e0743954c0c2c71409b0dd436babe
Vactor0911 <vactor0911@gmail.com>
2026-01-06 21:27:58 +0900

    모바일 메뉴 표시를 위한 페이지 래퍼 컴포넌트 구현

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
+import PageWrapper from "../components/PageWrapper";
 
 const Home = () => {
   return (
-    <>
+    <PageWrapper>
       <Typography variant="h4">Welcome to JBank</Typography>
-    </>
+    </PageWrapper>
   );
 };
 
