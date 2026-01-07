948ff08bccbd1e8ad81985673dcab85f0fc923e8
Vactor0911 <vactor0911@gmail.com>
2026-01-07 20:29:14 +0900

    레이아웃 테스트용 요소 제거

--- react/src/pages/Home/HomePc.tsx
+++ react/src/pages/Home/HomePc.tsx
@@ -4,136 +4,30 @@ import Ads from "./Ads";
 
 const Home = () => {
   return (
-    <Stack width="100%" gap={5}>
-      <Stack direction="row" gap={5} flexWrap="wrap">
-        {/* 빠른 작업 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Tasks />
-        </Paper>
+    <Stack direction="row" width="100%" gap={5} flexWrap="wrap">
+      {/* 빠른 작업 섹션 */}
+      <Paper
+        elevation={0}
+        sx={{
+          minWidth: "400px",
+          flex: 1,
+          borderRadius: 5,
+        }}
+      >
+        <Tasks />
+      </Paper>
 
-        {/* 광고 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Ads />
-        </Paper>
-      </Stack>
-
-      <Stack direction="row" gap={5} flexWrap="wrap">
-        {/* 빠른 작업 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Tasks />
-        </Paper>
-
-        {/* 광고 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Ads />
-        </Paper>
-      </Stack>
-
-      <Stack direction="row" gap={5} flexWrap="wrap">
-        {/* 빠른 작업 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Tasks />
-        </Paper>
-
-        {/* 광고 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Ads />
-        </Paper>
-      </Stack>
-
-      <Stack direction="row" gap={5} flexWrap="wrap">
-        {/* 빠른 작업 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Tasks />
-        </Paper>
-
-        {/* 광고 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Ads />
-        </Paper>
-      </Stack>
-
-      <Stack direction="row" gap={5} flexWrap="wrap">
-        {/* 빠른 작업 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Tasks />
-        </Paper>
-
-        {/* 광고 섹션 */}
-        <Paper
-          elevation={0}
-          sx={{
-            minWidth: "400px",
-            flex: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Ads />
-        </Paper>
-      </Stack>
+      {/* 광고 섹션 */}
+      <Paper
+        elevation={0}
+        sx={{
+          minWidth: "400px",
+          flex: 1,
+          borderRadius: 5,
+        }}
+      >
+        <Ads />
+      </Paper>
     </Stack>
   );
 };
