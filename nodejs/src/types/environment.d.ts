5f1c48a2c6bdc59d36028dda474c9f04026edd18
Vactor0911 <vactor0911@gmail.com>
2026-01-09 21:16:32 +0900

    .env 환경변수 타입 정의

---
+++ nodejs/src/types/environment.d.ts
@@ -0,0 +1,31 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      CLIENT_URL: string;
+
+      // Database
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_DATABASE: string;
+
+      // Steam
+      STEAM_API_KEY: string;
+      STEAM_RETURN_URL: string;
+      STEAM_REALM: string;
+
+      // JWT
+      JWT_ACCESS_SECRET: string;
+      JWT_REFRESH_SECRET: string;
+      JWT_ACCESS_EXPIRES_IN: string;
+      JWT_REFRESH_EXPIRES_IN: string;
+
+      // Cookie
+      COOKIE_SECRET: string;
+    }
+  }
+}
+
+export {};
