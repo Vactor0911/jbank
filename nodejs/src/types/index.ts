09c562170bcf566e35beaf0142ad0f22b009461e
Vactor0911 <vactor0911@gmail.com>
2026-01-09 21:16:49 +0900

    JWT 유틸 함수 구현

---
+++ nodejs/src/types/index.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+
+/**
+ * 사용자 데이터 인터페이스
+ */
+export interface UserData {
+  uuid: string;
+  steamId: string;
+  steamName: string;
+  avatar: string;
+  profileUrl: string;
+  createdAt?: Date;
+  lastLogin?: Date;
+}
+
+/**
+ * JWT Access Token 페이로드 인터페이스
+ */
+export interface AccessTokenPayload {
+  userUuid: string;
+  userSteamId: string;
+  userSteamName: string;
+}
+
+/**
+ * JWT Refresh Token 페이로드 인터페이스
+ */
+export interface RefreshTokenPayload {
+  userUuid: string;
+}
+
+/**
+ * Steam 프로필 인터페이스
+ */
+export interface SteamProfile {
+  provider: string;
+  id: string;
+  displayName: string;
+  photos: Array<{ value: string }>;
+  _json: {
+    steamid: string;
+    profileurl: string;
+    personaname: string;
+    avatar: string;
+    avatarmedium: string;
+    avatarfull: string;
+  };
+}
+
+/**
+ * API 인증 요청 인터페이스
+ */
+export interface AuthRequest extends Request {
+  user?: AccessTokenPayload;
+}
+
+/**
+ * API 응답 인터페이스
+ */
+export interface APIResponse<T = any> extends Response {
+  success: boolean;
+  message: string;
+  data?: T;
+  accessToken?: string;
+}
