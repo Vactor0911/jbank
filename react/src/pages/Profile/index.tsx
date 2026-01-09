91be0b74b5abdf12792710364e79ca037ee678da
Vactor0911 <vactor0911@gmail.com>
2026-01-09 10:08:39 +0900

    스팀(Steam) 명칭 영어로 수정

--- react/src/pages/Profile/index.tsx
+++ react/src/pages/Profile/index.tsx
@@ -77,7 +77,7 @@ const Profile = () => {
                   fontWeight={500}
                   noWrap
                 >
-                  스팀 닉네임
+                  Steam 닉네임
                 </Typography>
                 <Typography variant="h5" noWrap>
                   백터 (Vactor0911)
@@ -91,7 +91,7 @@ const Profile = () => {
                   mt={2}
                   noWrap
                 >
-                  스팀 고유번호
+                  Steam 고유번호
                 </Typography>
                 <Typography variant="h5" noWrap>
                   123456789
