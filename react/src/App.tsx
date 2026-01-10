e524c414b7df1a286a2479091d427305bc000b88
Vactor0911 <vactor0911@gmail.com>
2026-01-11 06:02:46 +0900

    페이지 라우팅 구조 변경

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,63 +1,13 @@
-import { CssBaseline, Stack, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./utils/theme";
-import { BrowserRouter, Route, Routes } from "react-router";
-import {
-  AccountDetail,
-  Home,
-  Login,
-  NotFoundError,
-  Notice,
-  Profile,
-  TransactionDetail,
-  Transfer,
-} from "./pages";
-import NavigationBar from "./components/Navigation";
-import SidebarMenu from "./components/SidebarMenu";
-import Header from "./components/Header";
-import PageWrapper from "./components/PageWrapper";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
-
-      <BrowserRouter>
-        <Stack
-          width="100vw"
-          height="100dvh"
-          direction={{
-            xs: "column",
-            md: "row-reverse",
-          }}
-        >
-          {/* 헤더 */}
-          <Header />
-
-          <PageWrapper>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/account/:accountUuid" element={<AccountDetail />} />
-              <Route
-                path="/transaction/:transactionUuid"
-                element={<TransactionDetail />}
-              />
-              <Route path="/account" element={<NotFoundError />} />
-              <Route path="/notice" element={<Notice />} />
-              <Route path="/transfer" element={<Transfer />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/profile" element={<Profile />} />
-
-              <Route path="*" element={<NotFoundError />} />
-            </Routes>
-          </PageWrapper>
-
-          {/* PC용 사이드 바 메뉴 */}
-          <SidebarMenu />
-
-          {/* 네비게이션 바 */}
-          <NavigationBar />
-        </Stack>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 };
---
+++ react/src/router/index.tsx
@@ -0,0 +1,92 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+
+import apiClient from "../api/axios";
+import {
+  AccountDetail,
+  Home,
+  Login,
+  NotFoundError,
+  Notice,
+  Profile,
+  TransactionDetail,
+  Transfer,
+} from "../pages";
+import RootLayout from "./RootLayout";
+
+/**
+ * 인증이 필요한 경로 접근 시 인증 상태 확인
+ * @returns 리다이렉트 응답 또는 null
+ */
+const requireAuth = async () => {
+  const isAuthenticated = await apiClient.initialize();
+  if (!isAuthenticated) {
+    return redirect("/login");
+  }
+
+  return null;
+};
+
+/**
+ * 이미 인증된 사용자 접근 차단
+ * @returns 리다이렉트 응답 또는 null
+ */
+const checkAuth = async () => {
+  await apiClient.initialize();
+  if (apiClient.isAuthenticated()) {
+    return redirect("/");
+  }
+
+  return null;
+};
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "account/:accountUuid",
+        element: <AccountDetail />,
+        loader: requireAuth,
+      },
+      {
+        path: "transaction/:transactionUuid",
+        element: <TransactionDetail />,
+        loader: requireAuth,
+      },
+      {
+        path: "account",
+        element: <NotFoundError />,
+        // 인증 불필요 (에러 페이지)
+      },
+      {
+        path: "notice",
+        element: <Notice />,
+      },
+      {
+        path: "transfer",
+        element: <Transfer />,
+        loader: requireAuth,
+      },
+      {
+        path: "login",
+        element: <Login />,
+        loader: checkAuth, // 이미 로그인된 경우 홈으로
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+        loader: requireAuth,
+      },
+      {
+        path: "*",
+        element: <NotFoundError />,
+        // 인증 불필요 (에러 페이지)
+      },
+    ],
+  },
+]);
