ab9fa27ab6f971a634e9a8ec82707a28ad47be7a
Vactor0911 <vactor0911@gmail.com>
2026-01-19 19:59:15 +0000

    랜덤 값 생성 로직 리팩토링

--- nodejs/src/utils/index.ts
+++ nodejs/src/utils/index.ts
@@ -31,24 +31,12 @@ export const parseTimeToSeconds = (time: string): number => {
 };
 
 /**
- * 안전한 난수를 생성하는 함수
- * @param max 최대값
- * @returns 0 이상 max 미만의 정수
- */
-const getSecureRandomInt = (max: number): number => {
-  const randomBytes = crypto.randomBytes(4);
-  const randomInt = randomBytes.readUInt32BE(0);
-  return randomInt % max;
-};
-
-/**
  * 계좌 번호를 생성하는 함수
  * @returns XXXX-XXXX 형식의 계좌번호 (예: "1234-5678")
  */
 export const generateAccountNumber = (): string => {
-  const part1 = getSecureRandomInt(10000).toString().padStart(4, "0");
-
-  const part2 = getSecureRandomInt(10000).toString().padStart(4, "0");
+  const part1 = crypto.randomInt(0, 10000).toString().padStart(4, "0");
+  const part2 = crypto.randomInt(0, 10000).toString().padStart(4, "0");
 
   return `${part1}-${part2}`;
 };
